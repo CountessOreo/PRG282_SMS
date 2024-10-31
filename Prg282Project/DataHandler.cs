24321bf1c50bb82601ddb5a2ec2f69ad2df32397
Hayley Treutens <578003@student.belgiumcampus.ac.za>
2024-10-31 09:47:00 +0200

    Add new student & DataHandler created

---
+++ Prg282Project/DataHandler.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prg282Project
+{
+    internal class DataHandler
+    {
+        public string filename = @"students.txt";
+        public int totalStudents = 0;
+        public int averageStudentsAge = 0;
+
+        /// <summary>
+        /// Generates amount of students in students.txt
+        /// </summary>
+        public void AmountOfStudents()
+        {
+            // Read students file
+            if (File.Exists(filename))
+            {
+                using (StreamReader reader = new StreamReader(filename))
+                {
+                    string txt;
+                    while ((txt = reader.ReadLine()) != null)
+                    {
+                        totalStudents++;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Generates Average Age of students in students.txt
+        /// </summary>
+        public void AverageStudentAge()
+        {
+            string CurrentLine;
+            using (StreamReader reader = new StreamReader(filename))
+            {
+                reader.ReadLine();
+                while ((CurrentLine = reader.ReadLine()) != null)
+                {
+
+                    string[] Students = CurrentLine.Split(',');
+                    int age = int.Parse(Students[2]);
+                    averageStudentsAge += age;
+
+                }
+                averageStudentsAge = averageStudentsAge / totalStudents;
+            }
+        }
+    }
+    
+}
